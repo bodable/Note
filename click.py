b47bee106db1d6fff05b943320249f68e9379e3a
ATM@006 <18829897162@163.com>
2018-03-08 13:57:39 +0800

    add click.py

---
+++ click.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.5
+import pyautogui
+import time
+print("请在3秒内选择点击位置...")
+
+for i in range(1,4):
+	time.sleep(1)
+	print(i)
+
+a,b = pyautogui.position()
+
+print(a,b)
+
+for i in range(1,200):
+	pyautogui.click(a,b,button='left')
+	time.sleep(2)
+	print(i)
+
+
+
+

6c624918c4ce195309f2b118518a916dcac3a830
ATM006 <18829897162@163.com>
2018-03-13 09:44:14 +0800

    modify run.sh

--- auto/run.sh
+++ auto/run.sh
@@ -1,4 +1,23 @@
-sudo apt-get update	
-sudo apt-get upgrade
+#!/bin.sh
+
+##安装准备
+#更新软件列表
+sudo apt-get update
+#更新软件	
+#sudo apt-get upgrade
+
+
+##前期安装
+#vim
+echo "atm installing vim" 
+echo y|sudo apt-get install vim
+
+#chrome
+echo "atm installing chrom"
+sh chrome.sh
+
+ 
+
+
 
 

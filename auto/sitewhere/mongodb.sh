90149bc9f751fe71919b91a1c9a911d5b2193bac
ATM006 <18829897162@163.com>
2018-03-21 23:53:38 -0500

    mongo-fix

--- auto/sitewhere/mongodb.sh
+++ auto/sitewhere/mongodb.sh
@@ -6,6 +6,10 @@ echo "atm installing MongoDb"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
+
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+#echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
 sudo apt-get update
 
 sudo apt-get install -y mongodb-org
@@ -14,5 +18,6 @@ echo "mongodb-org-server hold" | sudo dpkg --set-selections
 echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
 service mongod start
 

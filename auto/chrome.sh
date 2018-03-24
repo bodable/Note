24f6664decd738bb61fc983d0d2082e87cabffdb
ATM006 <18829897162@163.com>
2018-03-24 13:40:07 +0800

    chrome

--- auto/chrome.sh
+++ auto/chrome.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #chrome
 
-sudo add-apt-repository ppa:a-v-shkop/chromium
+
+sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+
 sudo apt-get update
-echo y|sudo apt-get install chromium-browser
+echo y|sudo apt-get install google-chrome-stable
+

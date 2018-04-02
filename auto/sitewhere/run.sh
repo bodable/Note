041c00c3481ef4e95329ad09340532fb2fbcce05
ATM006 <18829897162@163.com>
2018-04-02 22:02:00 +0800

    -

--- auto/sitewhere/run.sh
+++ auto/sitewhere/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #deoloy sitewhere
+#sudo sh run.sh
 
 echo "atm starting sitewhere..."
 
 service mongod start
 
 cd /opt/hivemq-3.0.2/bin/
-sudo ./run.sh
+./run.sh
 
 sudo bash /opt/sitewhere/bin/startup.sh 
 
